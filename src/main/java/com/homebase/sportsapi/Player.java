fb38f1efa65b15bf4a1004b53c258659b4b89d76
Abhijit Kamat <avk41+blah@scarletmail.rutgers.edu>
2018-07-04 18:37:49 -0400

    constructors and getters/setters

--- src/main/java/com/homebase/sportsapi/Player.java
+++ src/main/java/com/homebase/sportsapi/Player.java
@@ -4,6 +4,49 @@ package com.homebase.sportsapi;
  * Player
  */
 public class Player {
+    private String firstName, lastName, position;
+    private int age;
 
-    
+    public Player(){
+
+    }
+
+    public Player(String firstName, String lastName, String position, int age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+        this.age = age;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public String getPosition(){
+        return position;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public void setPosition(String position){
+        this.position = position;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
 }
