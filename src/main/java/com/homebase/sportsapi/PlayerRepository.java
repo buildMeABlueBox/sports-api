e1001c9c9d647c03505ad8d770e236ba90c7bbe7
Abhijit Kamat <avk41+blah@scarletmail.rutgers.edu>
2018-07-06 23:25:38 -0400

    checkpoint for playerRepository

--- src/main/java/com/homebase/sportsapi/PlayerRepository.java
+++ src/main/java/com/homebase/sportsapi/PlayerRepository.java
@@ -1,25 +1,47 @@
-// package com.homebase.sportsapi;
+package com.homebase.sportsapi;
 
-// import com.mongodb.MongoClient;
-// import java.util.ArrayList;
-// import java.util.List;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
-// /**
-//  * PlayerRepository - class to interact with a repository (File/DB/etc)
-//  */
-// public class PlayerRepository {
-//     private MongoClient mongoClient;
-//     private List<Player> players;
+import org.bson.Document;
 
-//     public PlayerRepository(){
-//         mongoClient = new MongoClient();
-//     }
+import java.util.List;
 
-//     public List<Player> getPlayers(){
-//         return players;
-//     }
+/**
+ * PlayerRepository - class to interact with a repository (File/DB/etc)
+ */
+public class PlayerRepository {
+    private MongoClient mongo;
+    private MongoDatabase database;
+	private MongoCollection<Document> table;
+    private List<Player> players;
 
-//     // public Player getPlayer(int id){
-//     //     for(player a:)
-//     // }
-// }
+    public PlayerRepository(String sport){
+        mongo = new MongoClient();
+        database = mongo.getDatabase("sports");
+        setTable(database.getCollection(sport));
+    }
+
+    /**
+	 * @return the table
+	 */
+	public MongoCollection<Document> getTable() {
+		return table;
+	}
+
+	/**
+	 * @param table the table to set
+	 */
+	public void setTable(MongoCollection<Document> table) {
+		this.table = table;
+	}
+
+	public List<Player> getPlayers(){
+        return players;
+    }
+
+    // public Player getPlayer(int id){
+    //     for(player a:)
+    // }
+}
